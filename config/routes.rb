42caae0e12372adf5c6c282ba4cead96346cb6b5
Dusty211 <kylebenz@hotmail.com>
2019-02-09 18:50:43 -0500

    5 failures left

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :appointments
-  resources :patients
-  resources :doctors
+
   resources :patients
   resources :doctors
   resources :appointments, only: [:show]
